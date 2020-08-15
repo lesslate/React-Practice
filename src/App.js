2153fc2196ed5992b0e69829045e8efe529de158
ChanIk Kwon <39299919+lesslate@users.noreply.github.com>
2020-08-15 20:34:24 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+
+const foodILike =
+  [
+    {
+      id: 1,
+      name: "kimchi",
+      rating: 3.0
+    },
+    {
+      id=2,
+      name: "kimbap",
+      rating: 4.8
+    },
+    {
+      id=3,
+      name: "dduk",
+      rating: 2
+    }
+  ];
+
+
+function Food({ name })
+{
+  return <h1>i like {name}!</h1>
+}
+
+
+function App()
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} />
+      ))}
     </div>
   );
 }
